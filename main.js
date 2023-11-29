e0156afc625e42ca7197033ef697392971310102
Pietro <pietroabrahamian2018@gmail.com>
2023-11-28 23:13:45 -0300

    dia 28/11

---
+++ main.js
@@ -0,0 +1,21 @@
+window.addEventListener("scroll", onScroll);
+onScroll();
+
+// Função responsável por gerenciar as funções do scroll na pagina
+function onScroll() {
+  showNavOnScroll();
+  menuAtivado(agenda);
+  menuAtivado(sobre);
+  menuAtivado(resgates);
+  menuAtivado(ajudar);
+}
+
+function showNavOnScroll() {
+  const navigation = document.querySelector("#navigation");
+  const imagemLogo = document.querySelector("#imglogo");
+  if (scrollY > 10) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
+}
